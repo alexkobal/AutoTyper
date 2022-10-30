7f281dc6143246db86c3746422023e5ae20f66db
alexkobal <alexkobal90mail.com>
2022-10-30 11:56:54 +0100

    Removed Service because it is easyer to register hotkeys with a system tray application (can be handled from windows forms) Configured the application to run as a system tray application.

---
+++ AutoTyperGUI/AutoTyperApplicationContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoTyperGUI
+{
+    internal class AutoTyperApplicationContext : ApplicationContext
+    {
+        private NotifyIcon notifyIcon;
+        public AutoTyperApplicationContext()
+        {
+            MenuItem configMenuItem = new MenuItem("Configuration", new EventHandler(ShowConfig));
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(Exit));
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.AppIcon;
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[]
+                { configMenuItem, exitMenuItem });
+            notifyIcon.Visible = true;
+        }
+
+        Form1 configWindow = new Form1();
+        void ShowConfig(object sender, EventArgs e)
+        {
+            // If we are already showing the window, merely focus it.
+            if (configWindow.Visible)
+            {
+                configWindow.Activate();
+            }
+            else
+            {
+                configWindow.ShowDialog();
+            }
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // We must manually tidy up and remove the icon before we exit.
+            // Otherwise it will be left behind until the user mouses over.
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
--- AutoTyperGUI/Program.cs
+++ AutoTyperGUI/Program.cs
@@ -16,7 +16,7 @@ namespace AutoTyperGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AutoTyperApplicationContext());
         }
     }
 }
