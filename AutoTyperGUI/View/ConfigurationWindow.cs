42b925e1cf41970ef6259677f9cf0e07bbeac0b9
alexkobal <alexkobal90mail.com>
2022-11-25 15:19:05 +0100

    Cleaned the configuration window

--- AutoTyperGUI/View/ConfigurationWindow.cs
+++ AutoTyperGUI/View/ConfigurationWindow.cs
@@ -14,9 +14,6 @@ namespace AutoTyperGUI
     public partial class ConfigurationWindow : Form
     {
         private AutoTyper autoTyper = AutoTyper.Instance;
-        private bool keyCombButtonPressed = false;
-        private object button;
-        private List<Keys> keys = new List<Keys>();
         private SaveFileDialog saveFileDialog;
         private OpenFileDialog openFileDialog;
         public ConfigurationWindow()
@@ -26,67 +23,6 @@ namespace AutoTyperGUI
             saveFileDialog.DefaultExt = "xml";
             openFileDialog = new OpenFileDialog();
             openFileDialog.DefaultExt = "xml";
-            this.KeyPreview = true;
-            refreshTextBoxes();
-        }
-
-        private void refreshTextBoxes()
-        {
-            int i = 0;
-            foreach (Chunk c in autoTyper.Chunks)
-            {
-                (this.Controls.Find("chunk" + i + "textBox", true)[0]).Text = c.Text;
-                (this.Controls.Find("chunk" + i + "cliptextBox", true)[0]).Text = c.ClipboardKHook.ToString();
-                (this.Controls.Find("chunk" + i + "autotextBox", true)[0]).Text = c.AutoTypeKHook.ToString();
-                i++;
-            }
-        }
-
-        private void chunkSaveButton_Click(object sender, EventArgs e)
-        {
-            int idx = (int)Char.GetNumericValue(((Button)sender).Name[5]);
-            autoTyper.Chunks[idx].Text = (this.Controls.Find("chunk" + idx + "textBox", true)[0]).Text;
-        }
-
-        private void chunkHookButton_Click(object sender, EventArgs e)
-        {
-            if (!keyCombButtonPressed)
-            {
-                button = sender;
-                keyCombButtonPressed = true;
-            }
-            else
-            {
-                if (button.Equals(sender))
-                {
-                    int idx = (int)Char.GetNumericValue(((Button)sender).Name[5]);
-                    if (((Button)sender).Name.Substring(6, 4) == "clip")
-                    {
-                        autoTyper.Chunks[idx].ClipboardKHook = Chunk.createKeyboardHook(keys);
-                        (this.Controls.Find(((Button)sender).Name.Substring(0, 10) + "textBox", true)[0]).Text =
-                            (autoTyper.Chunks[idx].ClipboardKHook != null ? autoTyper.Chunks[idx].ClipboardKHook.ToString() : "");
-                    }
-                    else
-                    {
-                        autoTyper.Chunks[idx].AutoTypeKHook = Chunk.createKeyboardHook(keys);
-                        (this.Controls.Find(((Button)sender).Name.Substring(0, 10) + "textBox", true)[0]).Text =
-                            (autoTyper.Chunks[idx].AutoTypeKHook != null ? autoTyper.Chunks[idx].AutoTypeKHook.ToString() : "");
-                    }
-                    keys.Clear();
-                    keyCombButtonPressed = false;
-                }
-            }
-        }
-
-        private void ConfigurationWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (keyCombButtonPressed)
-            {
-                if (e.Modifiers != Keys.None)
-                    keys.Add(e.Modifiers);
-                else
-                    keys.Add(e.KeyCode);
-            }
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
@@ -103,7 +39,6 @@ namespace AutoTyperGUI
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 autoTyper.open(openFileDialog.FileName);
-                refreshTextBoxes();
             }
         }
 
