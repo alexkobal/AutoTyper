68d45b5ef47c27a5062ccf5ee07880c50ae6a2be
alexkobal <alexkobal90mail.com>
2022-11-26 15:46:40 +0100

    Prevent running multiple instances of the application

--- AutoTyperGUI/Program.cs
+++ AutoTyperGUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AutoTyperGUI
@@ -6,14 +7,27 @@ namespace AutoTyperGUI
     internal static class Program
     {
         /// <summary>
+        /// Mutex object to prevent running multiple instances of the application
+        /// </summary>
+        static Mutex mutex = new Mutex(true, "autoEnforceOneInstance");
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AutoTyperApplicationContext());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                // do the app code
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new AutoTyperApplicationContext());
+
+                // release mutex after the form is closed.
+                mutex.ReleaseMutex();
+                mutex.Dispose();
+            }
         }
     }
 }
